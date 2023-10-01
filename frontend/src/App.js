e681eb80c9c6eb65c0b85439c14ff9a3911c9dd0
mpascualach <miguelpacheson@hotmail.es>
2023-10-01 04:07:59 +0200

    integrated tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
-function App() {
+Modal.setAppElement("#root"); // for accessibility
+
+const App = () => {
+  const [isOpen] = useState(true);
+
+  const handleTransaction = () => {
+    console.log("Transaction started");
+  };
+
+  const connectWallet = () => {
+    // Logic to connect to the wallet
+    console.log("Connecting to wallet");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Modal
+        isOpen={isOpen}
+        contentLabel="Crypto and Chain Modal"
+        style={{
+          content: {
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+      >
+        {/* Chain Switch From */}
+        <div>
+          <label>Switch From Chain:</label>
+          <select>
+            <option value="Ethereum">Ethereum</option>
+            <option value="BSC">Binance Smart Chain</option>
+            // ... more options
+          </select>
+        </div>
+
+        {/* Chain Switch To */}
+        <div>
+          <label>Switch To Chain:</label>
+          <select>
+            <option value="Ethereum">Ethereum</option>
+            <option value="BSC">Binance Smart Chain</option>
+            // ... more options
+          </select>
+        </div>
+
+        {/* Token */}
+        <div>
+          <label>Token:</label>
+          <select>
+            <option value="BTC">BTC</option>
+            <option value="ETH">ETH</option>
+            // ... more options
+          </select>
+        </div>
+
+        {/* Amount */}
+        <div>
+          <label>Amount:</label>
+          <input type="number" placeholder="Amount" />
+        </div>
+
+        {/* Connect Wallet Button */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={connectWallet}
         >
-          Learn React
-        </a>
-      </header>
+          Connect to Wallet
+        </button>
+
+        {/* Transaction Button */}
+        <button onClick={handleTransaction}>Start Transaction</button>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
